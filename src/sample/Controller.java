1b80b50ec8bd99806e676543e7d8ce28364739c4
azravarnica <avarnica1@etf.unsa.ba>
2019-04-01 12:39:52 +0200

    Zadatak2

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -3,8 +3,14 @@ package sample;
 import javafx.event.ActionEvent
 import javafx.scene.control.TextArea
         import java.until.ArrayLista
+import java.util.ArrayList;
+
 public class Controller {
     public TextArea unos;
     public TextArea rezultat;
+    private ArrayList|<Artikal> artikli = new ArrayList<>();
+    public void DodajArtikle(ActionEvent actionEvent) {
+        string[]
+    }
 
 }
