e073683df07908be1319ee0e9fb3c868d7152ff7
azravarnica <avarnica1@etf.unsa.ba>
2019-04-01 12:33:06 +0200

    Zadatak2

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,10 @@
 package sample;
 
+import javafx.event.ActionEvent
+import javafx.scene.control.TextArea
+        import java.until.ArrayLista
 public class Controller {
+    public TextArea unos;
+    public TextArea rezultat;
+
 }
